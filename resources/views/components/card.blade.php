73ec45162f40f717d52c1ed788c3c33da10590c8
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-04 03:37:13 +0800

    dynamic size card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,9 @@
-<div class="flex w-full max-w-sm flex-col gap-4 rounded-md bg-slate-50 p-4 shadow shadow-green-300 sm:p-6 md:p-8">
+<div
+    class="{{ $size }} flex w-full flex-col gap-4 rounded-md bg-slate-50 p-4 shadow shadow-green-300 sm:p-6 md:p-8">
+
     <h4 class="text-3xl font-bold tracking-wider underline decoration-green-400 decoration-4 underline-offset-[10px]">
-        {{ $cardTitle }}</h4>
+        {{ $cardTitle }}
+    </h4>
+
     {{ $slot }}
 </div>
