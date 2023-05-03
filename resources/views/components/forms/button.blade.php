41482dc4cebf447511bcf55b2f9d955bad2353b6
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-04 03:12:45 +0800

    refactor class in button component

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,12 +1,7 @@
-@php
-    $btn = 'w-full rounded-md p-2 font-medium';
-    $class = match ($color) {
-        'primary' => "$btn bg-green-300 text-white hover:bg-green-400",
-        'danger' => "$btn bg-red-400 text-white hover:bg-red-500",
-        default => $btn,
-    };
-@endphp
-
-<button type="{{ $type ?? 'submit' }}" class="{{ $class }}">
+<button type="{{ $type ?? 'submit' }}" @class([
+    'w-full rounded-md p-2 font-medium',
+    'bg-green-300 text-white hover:bg-green-400' => $color === 'primary',
+    'bg-red-400 text-white hover:bg-red-500' => $color === 'danger',
+])>
     {{ $label }}
 </button>
