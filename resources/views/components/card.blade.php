a94aaf85e6add016e4721bcea0538752b9a96e30
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-04 02:50:30 +0800

    add bgColor and shadow on card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex w-full max-w-sm flex-col gap-4 p-4 sm:p-6 md:p-8">
+<div class="flex w-full max-w-sm flex-col gap-4 rounded-md bg-slate-50 p-4 shadow shadow-green-300 sm:p-6 md:p-8">
     <h4 class="text-3xl font-bold tracking-wider underline decoration-green-400 decoration-4 underline-offset-[10px]">
         {{ $cardTitle }}</h4>
     {{ $slot }}
