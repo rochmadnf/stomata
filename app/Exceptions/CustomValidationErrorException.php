04c1d43b5e434a77682a8df3394533e4b8a5c73e
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-05 03:43:41 +0800

    add parameter mode in validation request for return data

--- app/Exceptions/CustomValidationErrorException.php
+++ app/Exceptions/CustomValidationErrorException.php
@@ -7,13 +7,22 @@ use Exception;
 class CustomValidationErrorException extends Exception
 {
     protected $errors = [];
-    public function __construct($errors)
+    protected $mode = '';
+
+    public function __construct($errors, $mode)
     {
         $this->errors = $errors;
+        $this->mode = $mode;
     }
 
     public function render()
     {
+        if ($this->mode === 'api') {
+            return response()->json([
+                "message" => "failed",
+                "field" => $this->errors
+            ], 422);
+        }
         return back()->withErrors($this->errors)->withInput();
     }
 }
--- app/Traits/ControllerTrait.php
+++ app/Traits/ControllerTrait.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Validator;
 
 trait ControllerTrait
 {
-    public function isValidRequest(array $data, array $rules): bool
+    public function isValidRequest(array $data, array $rules, string $mode = 'web'): bool
     {
         $validator = Validator::make($data, $rules);
 
         if ($validator->fails()) {
-            throw new CustomValidationErrorException($validator->errors());
+            throw new CustomValidationErrorException($validator->errors(), $mode);
         }
 
         return true;
