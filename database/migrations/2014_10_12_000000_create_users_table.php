4a8ea9068d66dc34f44d768bd5667601bc3a2085
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-17 17:42:04 +0800

    add is_active column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_active')->default(false);
 
             $table->timestamps();
         });
