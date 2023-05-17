6a892249a5351573fbde0e2695be9c2039e9dfc1
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-17 21:42:54 +0800

    add id props in card blade

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div
+<div id="{{ $id ?? str()->random(10) }}"
     class="{{ $size }} flex w-full flex-col gap-4 rounded-md bg-slate-50 p-4 shadow shadow-green-300 sm:p-6 md:p-8">
 
     <h4 class="text-3xl font-bold tracking-wider underline decoration-green-400 decoration-4 underline-offset-[10px]">
