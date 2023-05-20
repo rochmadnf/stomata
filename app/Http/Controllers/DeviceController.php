2cdfb56be84502718264860e113840051d1c42a1
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-21 05:49:43 +0800

    store data from device to server

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->isValidRequest($request->all(), [
+            "device_token" => ['required', 'string', 'size:21', 'exists:devices,token'],
+            "filled"       => ['required', 'integer', 'min:0', 'max:100'],
+            "unfilled"     => ['required', 'integer', 'min:0', 'max:100'],
+        ], 'api');
+
+        $device = Device::with('user', 'deviceData')->where('token', $request->device_token)->first();
+
+        $device->deviceData()->create(["filled" => $request->filled, "unfilled" => $request->unfilled]);
+
+        return response()->json(["status" => "success", "message" => "Data berhasil disimpan."], 200);
+    }
+}
