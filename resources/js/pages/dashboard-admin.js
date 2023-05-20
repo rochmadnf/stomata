c7777acb52fe54c0a17e8bb457b76c896c911baf
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-21 05:50:25 +0800

    add value of filled and unfilled in marker dashboard page
    resolve #3

--- resources/js/pages/dashboard-admin.js
+++ resources/js/pages/dashboard-admin.js
@@ -20,6 +20,7 @@ window.addEventListener("load", async () => {
     const users = await axios.get(
         `${import.meta.env.VITE_APP_URL}/api/user-coords`
     );
+
     users.data.forEach((user) => {
         new mapboxgl.Marker({
             color: "#EF4444",
@@ -34,11 +35,17 @@ window.addEventListener("load", async () => {
                     offset: 35,
                 }).setHTML(
                     `<div class="flex flex-col gap-2 justify-center items-center">
-                        <strong class="text-lg select-none cursor-pointer hover:text-green-500 hover:border-b-green-500 text-gray-900 border-b-2 border-b-gray-700">${user.full_name}</strong>
+                        <strong class="text-lg select-none cursor-pointer hover:text-green-500 hover:border-b-green-500 text-gray-900 border-b-2 border-b-gray-700">${
+                            user.full_name
+                        }</strong>
                         <p>${user.address}</p>
                         <div class="flex gap-2 border border-slate-300 py-1 px-2 rounded-md">
-                            <p class="text-xs">Terisi: <strong class="font-bold text-gray-900">0%</strong></p>
-                            <p class="text-xs">Kosong: <strong class="font-bold text-gray-900">0%</strong></p>
+                            <p class="text-xs">Terisi: <strong class="font-bold text-gray-900">${
+                                user.device.last_device_data?.filled ?? 0
+                            }%</strong></p>
+                            <p class="text-xs">Kosong: <strong class="font-bold text-gray-900">${
+                                user.device.last_device_data?.unfilled ?? 0
+                            }%</strong></p>
                         </div>
                     </div>
                     `
