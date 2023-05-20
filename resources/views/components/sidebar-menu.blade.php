28fd37d12c5752433b4bc4ec1538bf93f46d2d93
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-20 17:55:08 +0800

    add extend class props

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -1,6 +1,6 @@
     <li>
         <a href="{{ $route }}"
-            class="{{ request()->is($id . '*') ? 'active-menu' : '' }} group flex items-center rounded-lg p-2 text-gray-600 transition duration-75 hover:bg-green-100 hover:text-green-500">
+            class="{{ request()->is($id . '*') ? 'active-menu' : '' }} {{ $extendClass ?? '' }} group flex items-center rounded-lg p-2 text-gray-600 transition duration-75 hover:bg-green-100 hover:text-green-500">
             {{ $slot }}
             <span class="ml-3">{{ ucwords($label) }}</span>
         </a>
