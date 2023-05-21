90df3d9258499af0ba7ba6286cd98b389e6cce1b
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-22 02:54:29 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,26 @@
-<x-main-layout subtitle="test">
-    <div class="text-2xl text-blue-600">abcd</div>
+<x-main-layout subtitle="test" :sidebar="false">
+    <div class="min-h-screen bg-green-50 bg-leaf-green-400/10">
+        <div class="relative isolate px-6 pt-14 lg:px-8">
+            <div class="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+                <div class="mb-8 flex justify-center">
+                    <div class="relative cursor-pointer rounded-full bg-white px-3 py-1 text-sm leading-6 text-gray-900">
+                        Rochmad Nurul Fahmi @ F55116037
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">STOMATA</h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-900">Aplikasi untuk memudahkan kamu dalam memantau
+                        kapasitas sampah milikmu.</p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <a href="{{ route('auth.iregister') }}"
+                            class="rounded-md bg-green-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700">Daftar
+                            Sekarang</a>
+                        <a href="{{ route('auth.ilogin') }}" class="text-sm font-semibold leading-6 text-gray-900">Login
+                            <spanaria-hidden="true">→</spanaria-hidden=>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Events\UpdateDeviceDataEvent;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
