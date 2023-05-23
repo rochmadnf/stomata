278d6cfa832b4adb1eeb90cdb5b25060c49e12b2
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-05-23 19:05:57 +0800

    make seeder user as super admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\User::create([
+            "full_name"    => "Super Admin",
+            "phone_number" => 0,
+            "gender"       => 1,
+            "address"      => "-",
+            "lat"          => 0,
+            "long"         => 0,
+
+            "email"     => "super_admin@stomata.test",
+            "password"  => bcrypt('P4$$w0rd'),
+            "is_admin"  => 1,
+            "is_active" => 1,
+        ]);
     }
 }
